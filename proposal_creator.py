084a77f551c722b7dd71893526404c1717a9b58f
Yisrael.Zhang <YisraelZhang@gmail.com>
2019-11-06 08:22:21 -0500

    add proposal creator

---
+++ proposal_creator.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from losses import iou
+from anchors import Anchors
+from nms import gpu_nms
+from bbox import bbox2loc, loc2bbox
+
+class ProposalCreator:
+    def __init__(self, parent_model, nms_threshold=0.7,
+                 n_train_pre_nms=12000, n_train_post_nms=2000,
+                 n_test_pre_nms = 6000, n_test_post_nms=300,
+                 min_size=16):
+        self.parent_model = parent_model
+        self.nms_threshold = nms_threshold
+        self.n_train_pre_nms = n_train_pre_nms
+        self.n_train_post_nms = n_train_post_nms
+        self.n_test_pre_nms = n_test_pre_nms
+        self.n_test_post_nms = n_test_post_nms
+        self.min_size = min_size
+
+    def __call__(self, loc, score,
+                 anchor, img_size, scale=1.):
+        if self.parent_model.training:
+            n_pre_nms = self.n_train_pre_nms
+            n_post_nms = self.n_train_post_nms
+        else:
+            n_pre_nms = self.n_test_pre_nms
+            n_post_nms = self.n_test_pre_nms
+
+        roi = loc2bbox(anchor, loc)
+        roi[:, 0] = torch.clamp_min(roi[:, 0], 0)
+        roi[:, 1] = torch.clamp_min(roi[:, 1], 0)
+        roi[:, 2] = torch.clamp_max(roi[:, 2], img_size[0])
+        roi[:, 3] = torch.clamp_max(roi[:, 3], img_size[1])
+
+        min_size = self.min_size
+        height = roi[:, 2] -roi[:, 0]
+        width = roi[:, 3] - roi[:, 1]
+        keep = (height > min_size) & (width > min_size)
+        roi = roi[keep, :]
+        score = score[keep]
+
+        if n_pre_nms > score.shape[0]:
+            order = score.sort(0, descending=True)
+            roi = roi[order[1][:, n_pre_nms], :]
+
+        keep_index, _ = gpu_nms(torch.stack((roi, score), dim=1), self.nms_threshold)
+        keep_index = keep_index[:, n_post_nms]
+
+        roi = roi[keep_index, :]
+
+        return roi
